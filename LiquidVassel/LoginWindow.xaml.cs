f7bd62589d73690aa268aee46e04b320de5b2191
fredHwilliams <frederique.h.williams@gmail.com>
2017-05-31 08:52:57 +0200

    try catch + vsl image + Close( )

--- LiquidVassel/LoginWindow.xaml.cs
+++ LiquidVassel/LoginWindow.xaml.cs
@@ -30,28 +30,38 @@ namespace LiquidVassel
 
         private void btnSign_Click(object sender, RoutedEventArgs e)
         {
-            using (var connect = new VesselModel())
-            {
-                string login = txtId.Text;
-                string passWord = pswPassword.Password;
-                
 
-                var loginQuery = (from ar in connect.Armateurs
-                                  where login == ar.LoginArma
-                                  where passWord == ar.Motdepassearma
-                                  select ar).ToList();
-                
-                if (loginQuery.Count==1)
+            try
+            {
+                using (var connect = new VesselModel())
                 {
-                    MessageBox.Show("Vous êtes connecté!");
-                    Page VueDashboard = new Page();
-                    this.NavigationService.Navigate(VueDashboard);
-                }
-                else
-                {
-                    MessageBox.Show("You shall not pass !");
+                    string login = txtId.Text;
+                    string passWord = pswPassword.Password;
+
+
+                    var loginQuery = (from ar in connect.Armateurs
+                                      where login == ar.LoginArma
+                                      where passWord == ar.Motdepassearma
+                                      select ar).ToList();
+
+                    if (loginQuery.Count == 1)
+                    {
+                        MessageBox.Show("Vous êtes connecté!");
+                        Page VueDashboard = new Page();
+                        this.NavigationService.Navigate(VueDashboard);
+                    }
+                    else
+                    {
+                        MessageBox.Show("You shall not pass !");
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Votre délai d'attente est dépassé \n"+ ex.Message);
+            }
+            
         }
 
         /// <summary>
@@ -65,7 +75,7 @@ namespace LiquidVassel
             MessageBoxResult msgSortie = MessageBox.Show("Are you sure ?", "Exit", MessageBoxButton.YesNoCancel);
             if (msgSortie == MessageBoxResult.Yes)
             {
-                //this.Close();
+                Environment.Exit(0);
             }
         }
 
